1e5c2b49d58bba24714958a80aa119e7114ed3ad
Patton Parham <patton.parham@verizon.net>
2020-03-21 13:45:23 -0500

    fixing heroku, mongo serverjs issues

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = 3000;
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds345597.mlab.com:45597/heroku_tsnptqdr", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
