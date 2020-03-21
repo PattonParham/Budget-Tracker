7175e96549c95c83ddcc087685188cf763f30992
Patton Parham <patton.parham@verizon.net>
2020-03-21 13:33:52 -0500

    added mongo for heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds345597.mlab.com:45597/heroku_tsnptqdr", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
